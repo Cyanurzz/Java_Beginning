e199e39499df02525a8fcbc4da7683ea102e14ba
Jean DUCELLIER <jean@MacBook-Pro-de-Jean.local>
2019-11-02 19:16:40 +0100

    quest 03 Java checked

---
+++ 03.Les Variables/film.java
@@ -0,0 +1,13 @@
+class film {
+
+  public static void main(String[] args) {
+    String originalTitle = "Indiana Jones and the Last Crusade";
+    System.out.println(originalTitle);
+    boolean seen = false;
+    System.out.println(seen);
+    int theatricalRelease = 1989;
+    System.out.println(theatricalRelease);
+    float score = 8.2f;
+    System.out.println(score);
+  }
+}
