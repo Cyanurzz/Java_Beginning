1e8322ae4407652ac5630d032df78e36a55fb193
Jean DUCELLIER <jean@MacBook-Pro-de-Jean.local>
2019-11-02 19:24:55 +0100

    Revert "quest 06 Java checked"
    This reverts commit 6303cf0bf6c8c63c2f12fe1b6a701186dfa44cf5.

--- Classroom.java
+++
@@ -1,9 +0,0 @@
-public class Classroom {
-
-  public static void main(String[] args) {
-    Wilder jean = new Wilder("Jean", true);
-    System.out.println(jean.whoAmI());
-    Wilder poney = new Wilder("Kartoffel", false);
-    System.out.println(poney.whoAmI());
-  }
-}
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Affichage du Terminal a l'éxécution du code   
-![Image De Vérif](https://image.noelshack.com/fichiers/2019/40/1/1569833808-capture-d-ecran-2019-09-30-a-10-56-28.png)    
-_Première Approche de la POO à La WCS en Java_
+# Java_Beginning
