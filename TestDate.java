a80f64bc4394cf184755e5f0354c9b39fce21587
Jean DUCELLIER <jean@MacBook-Pro-de-Jean.local>
2019-11-12 11:44:27 +0100

    finish date exercice

---
+++ TestDate.java
@@ -0,0 +1,29 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+import java.util.Scanner; 
+
+class TestDate {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please fill a day (1-31) :");
+        int day = sc.nextInt();
+        System.out.println("Please fill a month (1-12) :");
+        int month = sc.nextInt();
+        System.out.println("Please fill a year :");
+        int year = sc.nextInt();
+        sc.close();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy"); 
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        Date d = c.getTime();
+        
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("The date is : " + sdf.format(d) + " \n The day of the week is : " + dayOfWeek);
+        
+    }
+}
