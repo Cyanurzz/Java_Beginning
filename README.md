1f293e4827151f6277e374a42ce64845d11f9a38
CyanurZz <jeanducellier.dev@gmail.com>
2020-01-05 16:22:06 +0100

    Spring Security quest

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Java_Beginning
+# Spring-Security
---
+++ src/main/java/com/wildcodeschool/security/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.wildcodeschool.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String shield() {
+        return "Welcome to the Shield";
+    }
+    
+    @GetMapping("/avengers/assemble")
+    public String avengers() {
+        return "Avengers... Assemble";
+    }
+    
+    @GetMapping("/secret-bases")
+    public String bases() {
+		return  "Amsterdam, Barcelone, Berlin, Bruxelles, Bucarest, Budapest, Dublin, Lisbonne, Londres, Madrid, Milan, Biarritz, Bordeaux, La Loupe 🌲, Lille, Lyon, Marseille, Nantes, Orléans, Paris, Reims, Strasbourg, Toulouse, Tours";
+    }
+}
